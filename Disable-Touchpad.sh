5777283e44c6337e3f925c809ef969f68cee1a9a
Hadronization <hadron.tablets@gmail.com>
2015-08-02 20:39:09 +0800

    Create Disable-Touchpad.sh

---
+++ Disable-Touchpad.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Listen for keyboard inputs & disable touchpad for 1s. 
+
+while :
+do
+	# read -t 1 -n 1 key 
+	key=$(xinput --query-state 11 | grep 'down')
+	
+	if [[ $key ]] 
+	then
+		xinput set-prop 12 "Device Enabled" 0	
+		sleep 1
+		xinput set-prop 12 "Device Enabled" 1
+	fi
+done
+
+# Ideas taken from various sources. Thanks Obama! 
+
